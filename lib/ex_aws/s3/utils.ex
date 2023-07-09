6adb5e76a1f6b008aeac3d7f5649575a7f5c72b3
Chris Laskey <contact@chrislaskey.com>
2023-07-08 21:00:54 -0400

    Mirror changes in factsfinder/ex_aws_s3

--- lib/ex_aws/s3/utils.ex
+++ lib/ex_aws/s3/utils.ex
@@ -47,7 +47,7 @@ defmodule ExAws.S3.Utils do
     regular_headers
     |> Map.merge(amz_headers)
     |> Map.merge(storage_class_headers)
-    |> Map.merge(acl_headers)
+    # |> Map.merge(acl_headers)
     |> Map.merge(encryption_headers)
     |> Map.merge(meta)
   end
